25cc9210bbb7d16b316b573e34b9f4fcf389890c
Anatolii1996 <anatoly.tka4enko2014@gmail.com>
2023-02-23 19:13:50 +0200

    подключил bootstrap

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
-// import axios from 'axios';
+import axios from 'axios';
 import { useEffect } from 'react';
-
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
 import './App.scss';
 
 function App() {
   useEffect(() => {
-    fetch("http://localhost:3001/posts")
-      .then((response) => response.json())
-      .then((json) => console.log((json)));
+    axios("http://localhost:3001/posts")
+      .then((json) => console.log((json.data)));
   }, [])
   return (
     <div className="App">
-
+      <div className="card" >
+        <img src="..." className="card-img-top" alt="..."/>
+          <div className="card-body">
+            <h5 className="card-title">Card title</h5>
+            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+           
+          </div>
+      </div>
     </div>
   );
 }
