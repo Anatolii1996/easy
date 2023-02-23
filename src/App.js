1ec27299d73303da6629722064d0cf221fcbaa55
Anatolii1996 <anatoly.tka4enko2014@gmail.com>
2023-02-23 21:58:49 +0200

    добавил постоянный хедер

--- src/App.js
+++ src/App.js
@@ -3,13 +3,16 @@ import './App.scss';
 import { Routes, Route } from "react-router";
 import Home from "./pages/Home";
 import Detail from "./pages/Detail";
+import Header from "./components/Header";
 
 function App() {
-  
+
   return (
     <Routes>
-      <Route path="/" element={<Home/>}/>
-      <Route path="/card" element={<Detail/>}/>
+      <Route path="/" element={<Header />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/card" element={<Detail />} />
+      </Route>
     </Routes>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,16 @@
-import { Link } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 
 const Header = () => {
   return (
+    <>
     <nav className="navbar bg-body-tertiary header">
       <div className="container-fluid ">
         <Link className="navbar-brand mb-0 h1" to="/">SHARETRADE.com</Link>
       </div>
     </nav>
+    <Outlet/>
+    </>
+    
   );
 };
 export default Header;
