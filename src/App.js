bd11eed08abef1075e15df8b786cf58262f41ff8
Anatolii1996 <anatoly.tka4enko2014@gmail.com>
2023-02-23 17:37:42 +0200

    обновил fetch

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+// import axios from 'axios';
+import { useEffect } from 'react';
+
 import './App.scss';
 
 function App() {
+  useEffect(() => {
+    fetch("http://localhost:3001/posts")
+      .then((response) => response.json())
+      .then((json) => console.log((json)));
+  }, [])
   return (
     <div className="App">
-     
+
     </div>
   );
 }
