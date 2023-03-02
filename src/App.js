7991b2607ca38c43ed3ff019340dbc80d600e6a0
Anatolii1996 <anatoly.tka4enko2014@gmail.com>
2023-03-02 15:04:22 +0200

    Слил в мастер

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
 import './App.scss';
 import { Routes, Route } from "react-router";
 import Home from "./pages/Home";
-import Detail from "./pages/Detail";
+import Detail from "./pages/detail";
 import Header from "./components/Header";
 
 function App() {
